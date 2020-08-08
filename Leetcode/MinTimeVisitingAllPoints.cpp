ed461a3aa38d4d73dafd0dd4f658189dc112dd45
sweatpantsdanny <d.hamilton@ufl.edu>
2020-08-08 03:20:16 -0400

    adjacency list implemented for directed graphwq

---
+++ Leetcode/MinTimeVisitingAllPoints.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+#include <vector>
+#include <stdio.h>
+
+using std::vector;
+
+#define SOL 1
+
+#define X 0
+#define Y 1
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+#if SOL == 1
+        return Sol1(nums);
+#endif
+    }
+
+    
+    vector<int> Sol1(vector<int>& nums) {
+         
+    }
+};
